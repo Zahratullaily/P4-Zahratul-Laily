efb40fe473cd7cd3b0f0620b84fb120ec5647842
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:37:08 +0800

    Create Jobsheet 9c.cpp
    Buatlah program array sebagai karakter dengan menampilkan nama setiap mahasiswa

---
+++ Jobsheet 9c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+int main() { 
+printf("Nama: Zahratul laily \n");
+	printf("NIM : F1B019146 \n");
+	printf("Kelompok: 25 \n");
+int i; 
+char nama[6]={'z','a','h','r','a'}; 
+char nama2[6]="zahra"; 
+printf("Nama per karakter = "); 
+for(i=0;i<=5;i++) { printf("%c",nama[i]);
+} printf("\n"); 
+printf("Nama = %s\n\n",nama2); 
+return 0;
+}
