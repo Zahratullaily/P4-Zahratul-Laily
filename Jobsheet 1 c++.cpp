c30d2e82d4e1795543f60b1d05b4532344b18e83
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:14:05 +0800

    Create Jobsheet 1 c++.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing masi

---
+++ Jobsheet 1 c++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+main (){
+	cout<<"zahratul laily\n";
+	cout<<"F1B019146\n";
+	cout<<"kelompok 25\n";
+	
+char* my_array[9]={"f1b019123","f1b019124","f1b019125","f1b019127","f1b019146",};
+	
+	cout<<"nimmahasiswake- 1 = "<<my_array[0]<<"\n"; //riosarifudin
+	cout<<"nimmahasiswake- 2 = "<<my_array[1]<<"\n"; //rismaprasasti
+	cout<<"nimmahasiswake- 3 = "<<my_array[2]<<"\n"; //riyanfathurrahman
+	cout<<"nimmahasiswake- 4 = "<<my_array[3]<<"\n"; //rohuliman
+	cout<<"nimmahasiswake- 5 = "<<my_array[4]<<"\n"; //zahratullaily
+
+}
