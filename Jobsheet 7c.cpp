700582e185a807a3f11e0364f6c478a21c9f4f9b
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:31:39 +0800

    Create Jobsheet 7c.cpp
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas perulangan menggunakan NIM terakhir mahasiswa

---
+++ Jobsheet 7c.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main (){
+	printf("Nama: Zahratul laily \n");
+	printf("NIM : F1B019146 \n");
+	printf("Kelompok: 25 \n");
+int nim [8], i;
+for (i = 0; i<= 8; i++){
+printf ("Masukkannimke-%i ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
