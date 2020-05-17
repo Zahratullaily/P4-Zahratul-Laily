8255f62212b6a531dc774b7b1cc510d4872dfcea
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:36:11 +0800

    Create Jobsheet 8c++.cpp
    Buatlah program mengisi nilai array dan menampilkannnya dengan perulangan dengan indeks array dan batas pengulangan menggunakan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 8c++.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"zahratul laily\n";
+	cout<<"F1B019146\n";
+	cout<<"kelompok 25\n";
+	
+           //akhirannim 5
+	int x[5],i;
+	for(i=0;i<5;i++)
+	{
+		cout<<"masukannilai "<<i+1<<" : ";
+		cin>>x[i];
+	}
+	cout<<endl;
+	for(i=0;i<5;i++)
+	{
+	cout<<"nilaike "<<i+1<<" = "<<x[i]<<endl;
+}
+	return 0;
+}
