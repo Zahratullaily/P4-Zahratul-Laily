c36720e100625c16eae6b713009aacaae4087b1d
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:40:08 +0800

    Create Jobsheet 10c.cpp

---
+++ Jobsheet 10c.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+main (){
+	printf("Nama: Zahratul laily \n");
+	printf("NIM : F1B019146 \n");
+	printf("Kelompok: 25 \n");
+char nama [2][10][30];
+int i, j;
+for (i = 0; i< 2; i++){
+for (j = 1; j < 5; j++){
+printf ("nama my best friend [%d][%d] = ", i, j);
+scanf ("%s", &nama[i][j]);
+}
+}
+}
