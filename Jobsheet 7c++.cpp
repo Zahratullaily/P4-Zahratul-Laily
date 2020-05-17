ee09ce2a839e3efd8fe55190cfbda6ce6b3f6955
Zahratullaily <65151712+Zahratullaily@users.noreply.github.com>
2020-05-17 23:33:19 +0800

    Create Jobsheet 7c++.cpp
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas perulangan menggunakan NIM terakhir mahasiswa

---
+++ Jobsheet 7c++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	cout<<"zahratul laily\n";
+	cout<<"F1B019146\n";
+	cout<<"kelompok 25\n";
+	
+	int x[5],i;
+	for(i=0;i<5;i++)
+	{
+	cout<<"Masukkan NIM ke- "<<i+1<<" : ";
+	cin>>x[i];
+}
+return 0;
+}
